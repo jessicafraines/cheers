016ed3be03a881cb1febc8a538e10cc09315cc64
jessicafraines <jessicafraines@gmail.com>
2014-11-06 13:13:21 -0600

    added remove space from name

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
+function removeSpace(name){
+  var nameWithoutSpace = name.replace(/\s/g,'');
+  return nameWithoutSpace;
+}
+
 function nameDisplay(name){
   return "Your name is " + name + "!";
 }
 
 function cheerFor(name){
   var connector;
+  name = removeSpace(name);
   return name.toUpperCase().split('').map(function(letter){
-    console.log(letter);
-    if(letter === "A" || letter === "E" || letter === "F" || letter === "H" || letter === "I" || letter === "L" || letter === "M" || letter === "N" || letter === "O" || letter === "R" || letter === "S" || letter === "U" || letter === "X"){
+    if(letter === "A" || letter === "E" || letter === "F" || letter === "H" || letter === "I" || letter === "L" || letter === "M" || letter === "N" || letter === "O" || letter === "R" || letter === "S" || letter === "X"){
       connector = "an";
     }else{
       connector = "a";
@@ -19,3 +24,13 @@ function closing(name){
   return name + " is Great!";
 }
 
+document.addEventListener('DOMContentLoaded', function(){
+  var $form = document.getElementById('enterName');
+  var $nameBox = $form.querySelector('input[type=text]');
+  $form.addEventListener('submit', function(event){
+    event.preventDefault();
+    nameDisplay($nameBox.value);
+    cheerFor($nameBox.value);
+    closing($nameBox.value);
+  });
+});
--- scripts/test.js
+++ scripts/test.js
@@ -11,10 +11,14 @@ q.test("cheerFor(name)", function(assert){
   assert.deepEqual(cheerFor("Bob"), expectedCheer);
   expectedCheer = "Give me a J!\nGive me an E!\nGive me an S!\nGive me an S!";
   assert.deepEqual(cheerFor("Jess"), expectedCheer);
+  expectedCheer = "Give me a J!\nGive me an O!\nGive me a J!\nGive me an O!";
+  assert.deepEqual(cheerFor("Jo Jo"), expectedCheer);
 });
 
 q.test("display name is great", function(assert){
   assert.equal(closing("Jessica"), "Jessica is Great!");
 });
 
-
+q.test("remove space from name", function(assert){
+  assert.equal(removeSpace("Sally Sue", " "), "SallySue"); 
+});
